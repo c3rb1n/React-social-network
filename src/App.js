128ba62d9eed7ec84a0f9360e2fd78b61edc901d
c3rb1n <ilya_prudnikov97@mail.ru>
2020-08-06 18:10:52 +0300

    remake app.js file using Header and Technologies components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import './App.css';
 const App = () => {
     return (
         <div>
-            <div>first div</div>
-            <div className="App">
-                <ul>
-                    <li>css</li>
-                    <li>html</li>
-                    <li>js</li>
-                    <li>react</li>
-                </ul>
-            </div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+};
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+};
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
         </div>
     );
 };
