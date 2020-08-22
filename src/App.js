b4eaff937997c4d84bf957bfd535b727c5972d89
c3rb1n <ilya_prudnikov97@mail.ru>
2020-08-22 22:17:47 +0300

    remove the training components Footer Header and Technologies, also add a simple layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
+
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+                    alt="logo"/>
+            </header>
+            <nav className="nav">
+                <div><a href="#">Profile</a></div>
+                <div><a href="#">Messages</a></div>
+                <div><a href="#">News</a></div>
+                <div><a href="#">Music</a></div>
+                <div><a href="#">Settings</a></div>
+            </nav>
+            <section className="content">
+                <div>
+                    <img src="https://www.cg.nl/wp-content/uploads/2018/06/tech-header-01.jpg" alt="content-img"/>
+                </div>
+                <div>avatar + description</div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>
+                        <div>Post1</div>
+                        <div>Post2</div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
