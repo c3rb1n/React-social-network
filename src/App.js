eded290187c770cb8731db72704e9eeec7a33e5d
c3rb1n <ilya_prudnikov97@mail.ru>
2020-08-23 06:27:34 +0300

    add Header, Navbar and Profile components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
 const App = () => {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
-                    alt="logo"/>
-            </header>
-            <nav className="nav">
-                <div><a href="#">Profile</a></div>
-                <div><a href="#">Messages</a></div>
-                <div><a href="#">News</a></div>
-                <div><a href="#">Music</a></div>
-                <div><a href="#">Settings</a></div>
-            </nav>
-            <section className="content">
-                <div>
-                    <img src="https://www.cg.nl/wp-content/uploads/2018/06/tech-header-01.jpg" alt="content-img"/>
-                </div>
-                <div>avatar + description</div>
-                <div>
-                    My posts
-                    <div>New post</div>
-                    <div>
-                        <div>Post1</div>
-                        <div>Post2</div>
-                    </div>
-                </div>
-            </section>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <section className="content">
+            <div>
+                <img src="https://www.cg.nl/wp-content/uploads/2018/06/tech-header-01.jpg" alt="content-img"/>
+            </div>
+            <div>avatar + description</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Profile;
