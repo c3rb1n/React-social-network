8b2a5509184248b01b8eab4804a9dfe94b1b28fb
c3rb1n <ilya_prudnikov97@mail.ru>
2020-08-29 22:50:42 +0300

    add layout for the Dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import classes from "./Dialogs.module.css";
 
 const Dialogs = props => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={`${classes.dialog} ${classes.active}`}>
+                    Ilya
+                </div>
+                <div className={classes.dialog}>
+                    Vitaly
+                </div>
+                <div className={classes.dialog}>
+                    Sergey
+                </div>
+                <div className={classes.dialog}>
+                    Svyatoslav
+                </div>
+                <div className={classes.dialog}>
+                    Alexander
+                </div>
+                <div className={classes.dialog}>
+                    Dmitry
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi!</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>What are you doing?</div>
+            </div>
         </div>
     );
 };
