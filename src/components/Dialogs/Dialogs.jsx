553b27d8c32d3e567854675c528f4c6028221d9d
c3rb1n <ilya_prudnikov97@mail.ru>
2020-09-04 10:13:02 +0300

    add UI-BLL practice

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,23 +19,42 @@ const Message = props => {
 };
 
 const Dialogs = props => {
+
+    let dialogsData = [
+        {id: 1, name: "Ilya"},
+        {id: 2, name: "Vitaly"},
+        {id: 3, name: "Sergey"},
+        {id: 4, name: "Svyatoslav"},
+        {id: 5, name: "Alexander"},
+        {id: 6, name: "Dmitry"}
+    ];
+
+    let messagesData = [
+        {id: 1, message: "Hi!"},
+        {id: 2, message: "How are you?"},
+        {id: 3, message: "What are you doing?"},
+        {id: 4, message: "Hello!"},
+        {id: 5, message: "Hello!"},
+        {id: 6, message: "Hello!"}
+    ];
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name="Ilya" id="1"/>
-                <DialogItem name="Vitaly" id="2"/>
-                <DialogItem name="Sergey" id="3"/>
-                <DialogItem name="Svyatoslav" id="4"/>
-                <DialogItem name="Alexander" id="5"/>
-                <DialogItem name="Dmitry" id="6"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
             </div>
             <div className={classes.messages}>
-                <Message message="Hi!"/>
-                <Message message="How are you?"/>
-                <Message message="What are you doing?"/>
-                <Message message="Hello!"/>
-                <Message message="Hello!"/>
-                <Message message="Hello!"/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
+                <Message message={messagesData[3].message}/>
+                <Message message={messagesData[4].message}/>
+                <Message message={messagesData[5].message}/>
             </div>
         </div>
     );
