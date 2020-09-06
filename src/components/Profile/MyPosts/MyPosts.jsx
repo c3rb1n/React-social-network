0b35a4d023ba5d65087b090e2a332fc68da5765d
c3rb1n <ilya_prudnikov97@mail.ru>
2020-09-06 07:00:02 +0300

    transfer data to the Profile component and add it to the MyPosts component via props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,15 +3,7 @@ import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = props => {
-
-    let posts = [
-        {id: 1, message: "Hi, how are you?", likesCount: "15"},
-        {id: 2, message: "It's my first post.", likesCount: "20"},
-        {id: 3, message: "Hi!", likesCount: "45"},
-        {id: 4, message: "Hello!", likesCount: "8"}
-    ];
-
-    let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
+    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 
     return (
         <div className={classes.postsBlock}>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,10 +4,18 @@ import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = props => {
+
+    let posts = [
+        {id: 1, message: "Hi, how are you?", likesCount: "15"},
+        {id: 2, message: "It's my first post.", likesCount: "20"},
+        {id: 3, message: "Hi!", likesCount: "45"},
+        {id: 4, message: "Hello!", likesCount: "8"}
+    ];
+
     return (
         <section>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts posts={posts}/>
         </section>
     );
 };
