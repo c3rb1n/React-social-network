0b145d7574be761f3f9b0a645356161dc2ad332f
c3rb1n <ilya_prudnikov97@mail.ru>
2020-09-06 07:49:47 +0300

    transfer all data to the index.js file and add data to the components via props

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,27 +4,8 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
 const Dialogs = props => {
-
-    let dialogs = [
-        {id: 1, name: "Ilya"},
-        {id: 2, name: "Vitaly"},
-        {id: 3, name: "Sergey"},
-        {id: 4, name: "Svyatoslav"},
-        {id: 5, name: "Alexander"},
-        {id: 6, name: "Dmitry"}
-    ];
-
-    let messages = [
-        {id: 1, message: "Hi!"},
-        {id: 2, message: "How are you?"},
-        {id: 3, message: "What are you doing?"},
-        {id: 4, message: "Hello!"},
-        {id: 5, message: "Hello!"},
-        {id: 6, message: "Hello!"}
-    ];
-
-    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
-    let messagesElements = messages.map(m => <Message message={m.message}/>);
+    let dialogsElements = props.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = props.messages.map(m => <Message message={m.message}/>);
 
     return (
         <div className={classes.dialogs}>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,34 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+let posts = [
+    {id: 1, message: "Hi, how are you?", likesCount: "15"},
+    {id: 2, message: "It's my first post.", likesCount: "20"},
+    {id: 3, message: "Hi!", likesCount: "45"},
+    {id: 4, message: "Hello!", likesCount: "8"}
+];
+
+let dialogs = [
+    {id: 1, name: "Ilya"},
+    {id: 2, name: "Vitaly"},
+    {id: 3, name: "Sergey"},
+    {id: 4, name: "Svyatoslav"},
+    {id: 5, name: "Alexander"},
+    {id: 6, name: "Dmitry"}
+];
+
+let messages = [
+    {id: 1, message: "Hi!"},
+    {id: 2, message: "How are you?"},
+    {id: 3, message: "What are you doing?"},
+    {id: 4, message: "Hello!"},
+    {id: 5, message: "Hello!"},
+    {id: 6, message: "Hello!"}
+];
+
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <App posts={posts} dialogs={dialogs} messages={messages}/>
     </React.StrictMode>,
     document.getElementById("root")
 );
