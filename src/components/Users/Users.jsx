83ac154e499bb7b395fe9adccf9c89a898e4f93e
c3rb1n <ilya_prudnikov97@mail.ru>
2020-09-22 02:28:24 +0300

    add splitting to presentational and container components on the Users page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,55 +1,36 @@
 import React from "react";
 import classes from "./Users.module.css";
 import User from "./User/User";
-import * as axios from "axios";
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-            });
+const Users = props => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    onPageChanged = pageNumber => {
-        this.props.setCurrentPage(pageNumber);
-
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-            });
-    }
-
-    render = () => {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        let pages = [];
-
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return (
-            <div>
-                <div className={classes.pages}>
-                    {pages.map(p => {
-                        return <button className={this.props.currentPage === p && classes.selectedPage}
-                                       onClick={() => this.onPageChanged(p)}>{p}</button>
-                    })}
-                </div>
-                <div>
-                    {this.props.users.map(u => <User key={u.id}
-                                                     name={u.name}
-                                                     id={u.id}
-                                                     photo={u.photos.small}
-                                                     status={u.status}
-                                                     followed={u.followed}
-                                                     follow={this.props.follow}
-                                                     unfollow={this.props.unfollow}/>)}
-                </div>
+    return (
+        <div>
+            <div className={classes.pages}>
+                {pages.map(p => {
+                    return <button key={p.id}
+                                   className={props.currentPage === p && classes.selectedPage}
+                                   onClick={() => props.onPageChanged(p)}>{p}</button>
+                })}
             </div>
-        )
-    }
-}
+            <div>
+                {props.users.map(u => <User key={u.id}
+                                            name={u.name}
+                                            id={u.id}
+                                            photo={u.photos.small}
+                                            status={u.status}
+                                            followed={u.followed}
+                                            follow={props.follow}
+                                            unfollow={props.unfollow}/>)}
+            </div>
+        </div>
+    );
+};
 
 export default Users;
